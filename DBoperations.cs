60c3512dd635a42ebb4cc15ca9ecd5e16d2ebbb5
Madelyn Nicole O'Dell <maniodel@umail.iu.edu>
2017-03-07 22:42:09 -0500

    Newly edited files --
    Registration is now connected to the database, and DBoperations has most viable methods, it should be noted that there are still some bugs that disallow the creation of appointments, but I'll try to fix that as best as I can.  Put these files in your KBS_Webpages directory and overwrite the old files, as well, I've included my web config, it will allow for access to the database.

---
+++ DBoperations.cs
@@ -0,0 +1,294 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+
+using System.Data.SqlClient;
+using System.Web.Configuration;
+
+
+/// <summary>
+/// Summary description for DBoperations
+/// </summary>
+public class DBoperations
+{
+    //variable declaration
+    public static SqlConnection con;
+    public static String cs;
+
+    //constructor
+    public DBoperations()
+    {
+        cs = WebConfigurationManager.ConnectionStrings["localConnection"].ConnectionString;
+        con = new SqlConnection(cs);
+    }
+
+    //Function to count in database to verify if the email inserted by user already exists.
+    //used for login operation and for avoiding double entry in registration.
+    public int GetOneInt(string sql)
+    {
+        try
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand(sql, con);
+
+            int value = (int)cmd.ExecuteScalar();
+
+            con.Close();
+
+            return value;
+        }
+        catch (Exception err)
+        {
+            err.ToString();
+            return -9999;
+        }
+        finally
+        {
+            con.Close();
+
+        }
+    }//end of GetOneInt()
+
+
+    //function used to verify user credentials for login operation.
+    public static Boolean SecureVerifyStudent(string UserName, string Password)
+    {
+        cs = WebConfigurationManager.ConnectionStrings["localConnection"].ConnectionString;
+        con = new SqlConnection(cs);
+        try
+        {
+            con.Open();
+
+            SqlCommand cmd = new SqlCommand("KBS_COUNT_STUDENT", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.Add(new SqlParameter("@UserName", SqlDbType.NVarChar, 50));
+            cmd.Parameters["@UserName"].Value = UserName;
+
+            cmd.Parameters.Add(new SqlParameter("@Password", SqlDbType.NVarChar, 50));
+            cmd.Parameters["@Password"].Value = Password;
+
+            cmd.Parameters.Add(new SqlParameter("@count", SqlDbType.Int));
+            cmd.Parameters["@count"].Direction = ParameterDirection.Output;
+
+            cmd.ExecuteNonQuery();
+
+            int count = (int)cmd.Parameters["@count"].Value;
+            con.Close();
+
+            if (count == 1) //specify the exact expected number, instead of count > 0
+                return true;
+            else return false;
+        }
+        catch (Exception err)
+        {
+            err.ToString();
+            return false;
+        }
+        finally
+        {
+            con.Close();
+
+        }
+    }//end of SecureVerifyStudent
+
+
+
+    //function used to create student accounts.
+    public static Boolean CreateStudent(string FirstName, string LastName, string Email, string UserName, string Password, int Major, int Guest, string ID, string Phone)
+    {
+        cs = WebConfigurationManager.ConnectionStrings["localConnection"].ConnectionString;
+        con = new SqlConnection(cs);
+            try
+            {
+                con.Open();
+
+
+                SqlCommand cmd = new SqlCommand("KBS_INSERT_STUDENT", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.Add(new SqlParameter("@First", SqlDbType.NVarChar, 50));
+                cmd.Parameters["@First"].Value = FirstName;
+
+                cmd.Parameters.Add(new SqlParameter("@Last", SqlDbType.NVarChar, 50));
+                cmd.Parameters["@Last"].Value = LastName;
+
+                cmd.Parameters.Add(new SqlParameter("@Email", SqlDbType.NVarChar, 50));
+                cmd.Parameters["@Email"].Value = Email;
+
+                cmd.Parameters.Add(new SqlParameter("@UserName", SqlDbType.NVarChar, 50));
+                cmd.Parameters["@UserName"].Value = UserName;
+
+                cmd.Parameters.Add(new SqlParameter("@Password", SqlDbType.NVarChar, 50));
+                cmd.Parameters["@Password"].Value = Password;
+
+                cmd.Parameters.Add(new SqlParameter("@Phone", SqlDbType.NVarChar, 50));
+                cmd.Parameters["@Phone"].Value = Phone;
+
+                cmd.Parameters.Add(new SqlParameter("@SMID", SqlDbType.NVarChar, 50));
+                cmd.Parameters["@SMID"].Value = Major;
+
+                cmd.Parameters.Add(new SqlParameter("@Prospective", SqlDbType.NVarChar, 50));
+                cmd.Parameters["@Prospective"].Value = Guest;
+
+                cmd.Parameters.Add(new SqlParameter("@SUID", SqlDbType.NChar, 10));
+                cmd.Parameters["@SUID"].Value = ID;
+
+                cmd.ExecuteNonQuery();
+                return true;
+
+            }
+            catch (Exception err)
+            {
+                err.ToString();
+                return false;
+            }
+            finally
+            {
+                con.Close();
+
+            }
+        
+
+    }//end of CreateStudent.
+
+
+    public static int ReturnStudentID(string username, string password)
+    {
+        cs = WebConfigurationManager.ConnectionStrings["localConnection"].ConnectionString;
+        con = new SqlConnection(cs);
+        try
+        {
+            con.Open();
+
+
+            SqlCommand cmd = new SqlCommand("KBS_GET_UID", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.Add(new SqlParameter("@UserName", SqlDbType.NVarChar, 50));
+            cmd.Parameters["@UserName"].Value = username;
+
+            cmd.Parameters.Add(new SqlParameter("@Password", SqlDbType.NVarChar, 50));
+            cmd.Parameters["@Password"].Value = password;
+
+            cmd.Parameters.Add(new SqlParameter("@UID", SqlDbType.Int));
+            cmd.Parameters["@UID"].Direction = ParameterDirection.Output;
+
+
+            cmd.ExecuteNonQuery();
+
+            int UID = (int)cmd.Parameters["@UID"].Value;
+            con.Close();
+
+            return UID;
+
+        }
+        catch (Exception err)
+        {
+            err.ToString();
+            return -1;
+        }
+        finally
+        {
+            con.Close();
+
+        }
+    }
+
+    //function used to create appointments.
+    public static Boolean CreateAppointment(int StudentId, int AdvisorTime)
+    {
+        cs = WebConfigurationManager.ConnectionStrings["localConnection"].ConnectionString;
+        con = new SqlConnection(cs);
+        try
+        {
+            con.Open();
+
+       
+            SqlCommand cmd = new SqlCommand("KBS_INSERT_APPOINTMENT", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+
+            cmd.Parameters.Add(new SqlParameter("@AdvisorTime", SqlDbType.NVarChar, 50));
+            cmd.Parameters["@AdvisorTime"].Value = AdvisorTime;
+            cmd.Parameters.Add(new SqlParameter("@Student", SqlDbType.NVarChar, 50));
+            cmd.Parameters["@Student"].Value = StudentId;
+
+            cmd.Parameters.Add(new SqlParameter("@Status", SqlDbType.NVarChar, 50));
+            cmd.Parameters["@Status"].Value = 3;
+
+            cmd.Parameters.Add(new SqlParameter("@Note", SqlDbType.NVarChar,200));
+            cmd.Parameters["@Note"].Value = "";
+
+            cmd.ExecuteNonQuery();
+            return true;
+
+        }
+        catch (Exception err)
+        {
+            err.ToString();
+            return false;
+        }
+        finally
+        {
+            con.Close();
+
+        }
+    }//end of CreateAppointment
+
+    public static Boolean CreateAdvisor(string First, string Last, string UserName, string Password, int Status)
+    {
+        cs = WebConfigurationManager.ConnectionStrings["localConnection"].ConnectionString;
+        con = new SqlConnection(cs);
+        try
+        {
+            con.Open();
+
+
+            SqlCommand cmd = new SqlCommand("KBS_INSERT_ADVISOR", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+
+            cmd.Parameters.Add(new SqlParameter("@first", SqlDbType.NVarChar, 50));
+            cmd.Parameters["@first"].Value = First;
+            cmd.Parameters.Add(new SqlParameter("@last", SqlDbType.NVarChar, 50));
+            cmd.Parameters["@last"].Value = Last;
+
+            cmd.Parameters.Add(new SqlParameter("@username", SqlDbType.NVarChar, 25));
+            cmd.Parameters["@username"].Value = UserName;
+
+            cmd.Parameters.Add(new SqlParameter("@password", SqlDbType.VarChar, 50));
+            cmd.Parameters["@password"].Value = Password;
+
+            cmd.Parameters.Add(new SqlParameter("@status", SqlDbType.Int));
+            cmd.Parameters["@status"].Value = Status;
+
+            cmd.ExecuteNonQuery();
+            return true;
+
+        }
+        catch (Exception err)
+        {
+            err.ToString();
+            return false;
+        }
+        finally
+        {
+            con.Close();
+
+        }
+    }//end of CreateAccount
+
+
+
+}
+
+
+
---
+++ registration.aspx.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+//importing asp.net libraries to handle the sql and database connections.
+using System.Data.SqlClient;
+using System.Web.Configuration;
+
+public partial class _Registration : System.Web.UI.Page
+{
+
+    //retrieving and saving to variables all the information in the form.
+    string FirstName = "";
+    string LastName = "";
+    string Email = "";
+    string Gender = "";
+    string Major = "";
+    string Phone = "";
+    string Advisor = "";
+    string TimeSlot = "";
+    string Reason = "";
+    string Date = "";
+    int StudentId;
+
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!Page.IsPostBack)
+        {
+            StudentInformation.Visible = true;
+            ContactInformation.Visible = false;
+
+            string cs = WebConfigurationManager.ConnectionStrings["localConnection"].ConnectionString;
+            SqlConnection con = new SqlConnection(cs);
+
+            //always use try/catch for db connections
+
+            try
+            {
+                ListItem newItem = new ListItem();
+                newItem.Text = "Select Major";
+                newItem.Value = "0";
+                Major_Form.Items.Add(newItem);
+
+                string selectSQL = "Select MajName,MID From VW_KBS_MAJORS";
+                SqlCommand cmd = new SqlCommand(selectSQL, con);
+                con.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    ListItem NewItem = new ListItem();
+                    NewItem.Text = reader["MajName"].ToString();
+                    NewItem.Value = reader["MID"].ToString();
+                    Major_Form.Items.Add(NewItem);
+                }
+                reader.Close();
+                con.Close();
+            }
+            catch (Exception err)
+            {
+                err.ToString();
+
+            }
+            finally
+            {
+                con.Close();
+
+            }
+
+        }
+    }
+
+
+
+
+
+    //This is the event. When the user clicks submit, it will do the following:
+    protected void btnSubmit_Click(object sender, EventArgs e)
+    {
+        string Username = UserName_Form.Text;
+        string Password = Password_Form.Text;
+        int ATime = TimeSlots_Form.SelectedIndex; 
+        int ID = DBoperations.ReturnStudentID("Test", "Test");
+        lblInfo.Text = ID.ToString();
+    
+    }
+
+    protected void btnNext_Click(object sender, EventArgs e)
+    {
+        StudentInformation.Visible = false;
+        ContactInformation.Visible = true;
+
+        string First = FirstName_Form.Text;
+        string Last = LastName_Form.Text;
+        string Email = Email_Form.Text;
+        string UserName = UserName_Form.Text;
+        string Password = Password_Form.Text;
+        string Major = Major_Form.SelectedValue;
+        int Maj = Convert.ToInt32(Major);
+        string Phone = Phone_Form.Text;
+        string UID = SUID_Form.Text;
+
+        Boolean Student = DBoperations.SecureVerifyStudent(UserName, Password);
+        if (Student == false)
+        {
+            DBoperations.CreateStudent(First, Last, Email, UserName, Password, Maj, 1, UID, Phone);
+        }
+
+        //Advisors
+        string cs = WebConfigurationManager.ConnectionStrings["localConnection"].ConnectionString;
+        SqlConnection con = new SqlConnection(cs);
+        try
+        {
+            ListItem newItem = new ListItem();
+            newItem.Text = "Select Advisor";
+            newItem.Value = "0";
+            Advisor_Form.Items.Add(newItem);
+
+            string selectSQL = "Select AID ,AdvFName, AdvLName From KBS_ADVISORS";
+            SqlCommand comd = new SqlCommand(selectSQL, con);
+            con.Open();
+            SqlDataReader reads = comd.ExecuteReader();
+
+            while (reads.Read())
+            {
+                ListItem NewItem = new ListItem();
+                string first = reads["AdvFName"].ToString();
+                string last = reads["AdvLName"].ToString();
+                string full = first + " " + last;
+                NewItem.Text = full;
+                NewItem.Value = reads["AID"].ToString();
+                Advisor_Form.Items.Add(NewItem);
+            }
+            reads.Close();
+            con.Close();
+        }
+        catch (Exception err)
+        {
+            err.ToString();
+
+        }
+        finally
+        {
+            con.Close();
+
+        }
+
+
+        try
+        {
+            string selectSQL = "Select RID, ReaDesc FROM VW_KBS_REASONS";
+            SqlCommand com = new SqlCommand(selectSQL, con);
+            con.Open();
+            SqlDataReader reader = com.ExecuteReader();
+
+            while (reader.Read())
+            {
+                ListItem NewItem = new ListItem();
+                NewItem.Text = reader["ReaDesc"].ToString();
+                NewItem.Value = reader["RID"].ToString();
+                Reason_Form.Items.Add(NewItem);
+            }
+            reader.Close();
+            con.Close();
+        }
+        catch (Exception err)
+        {
+            err.ToString();
+
+        }
+        finally
+        {
+            con.Close();
+
+        }
+
+
+
+
+    }
+
+
+    protected void drpChange_Time(object sender, EventArgs e)
+    {
+        TimeSlots_Form.Items.Clear();
+        
+        string cs = WebConfigurationManager.ConnectionStrings["localConnection"].ConnectionString;
+        SqlConnection con = new SqlConnection(cs);
+        try
+        {
+            ListItem newItem = new ListItem();
+            newItem.Text = "Select Time";
+            newItem.Value = "0";
+            TimeSlots_Form.Items.Add(newItem);
+
+            string selectSQL = "Select ATID ,AvaTime, AvaDate From KBS_ADVISOR_TIMES WHERE AID =" + Advisor_Form.SelectedIndex;
+            SqlCommand comd = new SqlCommand(selectSQL, con);
+            con.Open();
+            SqlDataReader reads = comd.ExecuteReader();
+
+            while (reads.Read())
+            {
+                ListItem NewItem = new ListItem();
+                string first = reads["AvaDate"].ToString();
+                string last = reads["AvaTime"].ToString();
+                string full = first + " " + last;
+                NewItem.Text = full;
+                NewItem.Value = reads["ATID"].ToString();
+                TimeSlots_Form.Items.Add(NewItem);
+            }
+            reads.Close();
+            con.Close();
+        }
+        catch (Exception err)
+        {
+            err.ToString();
+
+        }
+        finally
+        {
+            con.Close();
+
+        }
+
+    }
+
+}
